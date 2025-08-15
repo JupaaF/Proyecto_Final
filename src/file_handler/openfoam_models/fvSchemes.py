f25b859ec392b472597e96684c087bfd781041e8
marrrrrtv <pilivarrone@gmail.com>
2025-08-15 19:26:58 -0300

    fvSchemes

--- src/file_handler/openfoam_models/fvSchemes.py
+++ src/file_handler/openfoam_models/fvSchemes.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from .foam_file import FoamFile
 from jinja2 import Environment, FileSystemLoader
 
+#por ahora solo pueden configurarse los default!!!
+
 class fvSchemes(FoamFile):
 
     def __init__(self):
@@ -46,50 +48,67 @@ class fvSchemes(FoamFile):
         return {
             'ddtSchemes': {
                 'label': 'ddtSchemes',
-                'tooltip': 'Esquema de discretización temporal.',
-                'type': 'string',
+                'tooltip': '''Esquema de discretización para las primeras derivadas:
+                - steadyState: establece las derivadas temporales en cero.
+                - Euler: transitorio, implícito de primer orden, acotado.
+                - backward: transitorio, implícito de segundo orden, potencialmente ilimitado.
+                - CrankNicolson: transitorio, implícito de segundo orden, acotado; requiere un coeficiente de descentrado. 
+                  (Generalmente = 0,9 se utiliza para estabilizar el esquema en problemas prácticos de ingeniería.)
+                - localEuler: pseudotransitorio para acelerar una solución al estado estacionario mediante escalonamiento temporal local; implícito de primer orden.''',
+                'type': 'choice',
+                'options': ['steadyState','Euler','backward','CrankNicolson 0.9','localEuler'],
                 'current': self.ddtSchemes,
                 'group': 'Esquemas Temporales'
             },
             'gradSchemes': {
                 'label': 'gradSchemes',
-                'tooltip': 'Esquema de discretización para gradientes.',
-                'type': 'string',
+                'tooltip': '''Esquema de discretización para gradientes:
+                -  Gauss linear: especifica la discretización estándar de volumen finito con integración gaussiana.''',
+                'type': 'choice',
+                'options': ['Gauss linear'], # TODO ver si agregar lo de cellLimited (doc 4.5.2)
                 'current': self.gradSchemes,
                 'group': 'Esquemas de Gradiente'
             },
             'divSchemes': {
-                'label': 'divSchemes',
-                'tooltip': 'Esquema de discretización para divergencia.',
+                'label': 'divSchemes', # TODO generalizar esto, solo configuro del dambreak por ahora en el template (doc 4.5.3)
+                'tooltip': '''Esquema de discretización para divergencia (según el caso):
+                - damBreak: configuración correcta para este caso.''',
                 'type': 'choice',
                 'current': self.divSchemes,
                 'group': 'Esquemas de Divergencia',
                 'options': [
-                    'Gauss linear',
-                    'Gauss linearUpwind grad(U)',
-                    'Gauss upwind',
-                    'bounded Gauss upwind',
-                    'bounded Gauss linearUpwind grad(U)'
+                    'damBreak'
                 ]
             },
             'laplacianSchemes': {
                 'label': 'laplacianSchemes',
-                'tooltip': 'Esquema de discretización para laplacianos.',
-                'type': 'string',
-                'current': self.laplacianSchemes,
+                'tooltip': '''Esquema de discretización para laplacianos.
+En todos los casos, se utiliza el esquema de interpolación lineal para la interpolación de la difusividad. 
+Se utiliza la misma matriz de esquemas snGradSchemes basada en la no ortogonalidad máxima de la malla.''',
+                'type': 'choice',
+                'options': ['linear'],
+                'current': 'linear',
                 'group': 'Esquemas de Laplaciano'
             },
             'interpolationSchemes': {
                 'label': 'interpolationSchemes',
-                'tooltip': 'Esquema de interpolación.',
-                'type': 'string',
+                'tooltip': '''Esquema de interpolación.
+Existen numerosos esquemas en OpenFOAM, pero la interpolación lineal se utiliza en casi todos los casos.''',
+                'type': 'choice',
+                'options': ['linear'],
                 'current': self.interpolationSchemes,
                 'group': 'Esquemas de Interpolación'
             },
             'snGradSchemes': {
                 'label': 'snGradSchemes',
-                'tooltip': 'Esquema de discretización para gradientes de superficie normales.',
-                'type': 'string',
+                'tooltip': '''Los términos de gradiente normal son necesarios para evaluar un término laplaciano mediante integración gaussiana. 
+Un gradiente normal a la superficie se evalúa en una cara de celda.
+                              - uncorrected y orthogonal: solo se recomiendan para mallas con muy baja no ortogonalidad (p. ej., un máximo de 5 grados). 
+                              - corrected: se recomienda generalmente, pero...
+                              - limited: puede requerirse para una no ortogonalidad máxima por encima de 75 grados. 
+                              Con una no ortogonalidad superior a 85 eqn, la convergencia suele ser difícil de lograr.''',
+                'type': 'choice',
+                'options': ['corrected','uncorrected','orthogonal','limited corrected 0.33','limited corrected 0.5'],
                 'current': self.snGradSchemes,
                 'group': 'Esquemas de Gradiente de Superficie'
             }
